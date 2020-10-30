b0a2f0fa8262b7f0213c8d0e9b498992e262d92c
npomfret <nick-github@snowmonkey.co.uk>
2020-10-30 23:05:59 +0000

    basic liner regression working

--- website/src/App.js
+++ website/src/App.js
@@ -1,27 +1,104 @@
 import './App.css';
 import Sketch from "react-p5";
-import p5Types from "p5"; //Import this for typechecking and intellisense
-import {Container, Button, Card, Navbar, Row, Col, Form, FormGroup, InputGroup} from 'react-bootstrap';
+import {Card, Col, Container, Form, FormGroup, InputGroup, Navbar, Row} from 'react-bootstrap';
+import React, {useState} from 'react'
 
 // https://www.youtube.com/watch?v=JvS2triCgOY
 // https://www.youtube.com/watch?v=P8hT5nDai6A
 // https://github.com/CodingTrain/website/blob/main/Courses/intelligence_learning/session4/toy-neural-network-js/examples/xor/sketch.js
 
 const Doodle = () => {
-    let x = 50;
-    const y = 50;
+    const [data, setData] = useState([
+        // {x: 1, y: 3},
+        // {x: 2, y: 5},
+        // {x: 3, y: 7},
+        // {x: 4, y: 9},
+    ]);
 
-    const setup = (p5, canvasParentRef) => {
-        p5.createCanvas(500, 500).parent(canvasParentRef);
-    };
+    const width = 500;
+    const height = 500;
+
+    let m = 1;
+    let b = 0;
 
     const draw = (p5) => {
-        p5.background(0);
-        p5.ellipse(x, y, 70, 70);
-        x++;
+
+        function linearRegression() {
+            let xsum = 0;
+            let ysum = 0;
+            for (let i = 0; i < data.length; i++) {
+                xsum += data[i].x;
+                ysum += data[i].y;
+            }
+
+            const xmean = xsum / data.length;
+            const ymean = ysum / data.length;
+
+            let num = 0;
+            let den = 0;
+            for (let i = 0; i < data.length; i++) {
+                const x = data[i].x;
+                const y = data[i].y;
+                num += (x - xmean) * (y - ymean);
+                den += (x - xmean) * (x - xmean);
+            }
+
+            m = num / den;
+            b = ymean - m * xmean;
+        }
+
+        function drawLine() {
+            let x1 = 0;
+            let y1 = m * x1 + b;
+            let x2 = 1;
+            let y2 = m * x2 + b;
+
+            x1 = p5.map(x1, 0, 1, 0, width);
+            y1 = p5.map(y1, 0, 1, height, 0);
+            x2 = p5.map(x2, 0, 1, 0, width);
+            y2 = p5.map(y2, 0, 1, height, 0);
+
+            p5.stroke(255);
+            p5.strokeWeight(2);
+            p5.line(x1, y1, x2, y2);
+        }
+
+        p5.background(51);
+
+        for (let i = 0; i < data.length; i++) {
+            const x = p5.map(data[i].x, 0, 1, 0, width);
+            const y = p5.map(data[i].y, 0, 1, height, 0);
+            p5.fill(255);
+            p5.stroke(255);
+            p5.ellipse(x, y, 8, 8);
+        }
+
+        if (data.length > 1) {
+            linearRegression();
+            drawLine();
+        }
     };
 
-    return <Sketch setup={setup} draw={draw}/>;
+    return <div>
+        <Sketch
+            setup={(p5, canvasParentRef) => {
+                p5.createCanvas(width, height).parent(canvasParentRef);
+            }}
+            mousePressed={p5 => {
+                const x = p5.map(p5.mouseX, 0, width, 0, 1);
+                const y = p5.map(p5.mouseY, 0, height, 1, 0);
+                const point = p5.createVector(x, y);
+                data.push(point);
+                setData(data.slice())
+            }}
+            draw={draw}
+        />
+        <pre>
+            {JSON.stringify(data.map(point => {
+                return {x: point.x, y: point.y}
+            }), null, 2)}
+        </pre>
+    </div>
 };
 
 function App() {
@@ -41,7 +118,7 @@ function App() {
                                     Label
                                 </Form.Label>
                                 <InputGroup>
-                                    <Form.Control as="textarea" rows="3" name="address" onChange={() => console.log('s')} />
+                                    <Form.Control as="textarea" rows="3" name="address" onChange={() => console.log('s')}/>
                                 </InputGroup>
                             </FormGroup>
                         </Form>
