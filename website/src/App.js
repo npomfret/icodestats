705874bec7d46157a1eada4d98d262d4bc460272
npomfret <nick-github@snowmonkey.co.uk>
2020-10-30 23:21:10 +0000

    basic liner regression working

--- website/src/App.js
+++ website/src/App.js
@@ -18,36 +18,39 @@ const Doodle = () => {
     const width = 500;
     const height = 500;
 
-    let m = 1;
-    let b = 0;
 
     const draw = (p5) => {
 
         function linearRegression() {
-            let xsum = 0;
-            let ysum = 0;
-            for (let i = 0; i < data.length; i++) {
-                xsum += data[i].x;
-                ysum += data[i].y;
+            let m = 1;
+            let b = 0;
+
+            let xSum = 0;
+            let ySum = 0;
+
+            for (let {x, y} of data) {
+                xSum += x;
+                ySum += y;
             }
 
-            const xmean = xsum / data.length;
-            const ymean = ysum / data.length;
+            const sampleSize = data.length;
+            const xMean = xSum / sampleSize;
+            const yMean = ySum / sampleSize;
 
             let num = 0;
             let den = 0;
-            for (let i = 0; i < data.length; i++) {
-                const x = data[i].x;
-                const y = data[i].y;
-                num += (x - xmean) * (y - ymean);
-                den += (x - xmean) * (x - xmean);
+
+            for (let {x, y} of data) {
+                num += (x - xMean) * (y - yMean);
+                den += (x - xMean) * (x - xMean);
             }
 
             m = num / den;
-            b = ymean - m * xmean;
+            b = yMean - m * xMean;
+            return {m, b}
         }
 
-        function drawLine() {
+        function drawLine(m, b) {
             let x1 = 0;
             let y1 = m * x1 + b;
             let x2 = 1;
@@ -68,64 +71,71 @@ const Doodle = () => {
         for (let i = 0; i < data.length; i++) {
             const x = p5.map(data[i].x, 0, 1, 0, width);
             const y = p5.map(data[i].y, 0, 1, height, 0);
-            p5.fill(255);
-            p5.stroke(255);
+            p5.fill("#87ff5f");
+            p5.stroke("#87ff5f");
             p5.ellipse(x, y, 8, 8);
         }
 
         if (data.length > 1) {
-            linearRegression();
-            drawLine();
+            const {m, b} = linearRegression();
+            drawLine(m, b);
         }
     };
 
-    return <div>
-        <Sketch
-            setup={(p5, canvasParentRef) => {
-                p5.createCanvas(width, height).parent(canvasParentRef);
-            }}
-            mousePressed={p5 => {
-                const x = p5.map(p5.mouseX, 0, width, 0, 1);
-                const y = p5.map(p5.mouseY, 0, height, 1, 0);
-                const point = p5.createVector(x, y);
-                data.push(point);
-                setData(data.slice())
-            }}
-            draw={draw}
-        />
-        <pre>
-            {JSON.stringify(data.map(point => {
-                return {x: point.x, y: point.y}
-            }), null, 2)}
-        </pre>
-    </div>
+    return <Row>
+        <Col>
+            <Sketch
+                setup={(p5, canvasParentRef) => {
+                    p5.createCanvas(width, height).parent(canvasParentRef);
+                }}
+
+                mousePressed={p5 => {
+                    const x = p5.map(p5.mouseX, 0, width, 0, 1);
+                    const y = p5.map(p5.mouseY, 0, height, 1, 0);
+                    const point = p5.createVector(x, y);
+                    data.push(point);
+                    setData(data.slice())
+                }}
+
+                draw={draw}
+            />
+        </Col>
+
+        <Col>
+            <pre>
+                {JSON.stringify(data.map(point => {
+                    return {x: point.x, y: point.y}
+                }), null, 2)}
+            </pre>
+        </Col>
+
+        <Col>
+            <Card>
+                <Card.Body>
+                    <Form>
+                        <FormGroup>
+                            <Form.Label>
+                                code
+                            </Form.Label>
+                            <InputGroup>
+                                <Form.Control as="textarea" rows="3" name="address" onChange={() => console.log('s')}/>
+                            </InputGroup>
+                        </FormGroup>
+                    </Form>
+                </Card.Body>
+            </Card>
+        </Col>
+    </Row>
 };
 
 function App() {
-    return <Container fluid="lg" className="p-0">
+    return <Container fluid>
         <Navbar expand="lg" variant="light" bg="light">
             <Navbar.Brand href="#">Navbar</Navbar.Brand>
         </Navbar>
 
-        <Row>
-            <Col><Doodle/></Col>
-            <Col>
-                <Card>
-                    <Card.Body>
-                        <Form>
-                            <FormGroup>
-                                <Form.Label>
-                                    Label
-                                </Form.Label>
-                                <InputGroup>
-                                    <Form.Control as="textarea" rows="3" name="address" onChange={() => console.log('s')}/>
-                                </InputGroup>
-                            </FormGroup>
-                        </Form>
-                    </Card.Body>
-                </Card>
-            </Col>
-        </Row>
+        <Doodle/>
+
     </Container>
 }
 
