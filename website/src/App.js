61e3cbc2501c046a1221e7fcce95755fecbdbce9
Nick Pomfret <nick-github@snowmonkey.co.uk>
2020-10-30 20:35:46 +0000

    noodles

--- website/src/App.js
+++ website/src/App.js
@@ -1,24 +1,23 @@
 import './App.css';
 import Sketch from "react-p5";
 import p5Types from "p5"; //Import this for typechecking and intellisense
+import {Container, Button, Card, Navbar, Row, Col, Form, FormGroup, InputGroup} from 'react-bootstrap';
 
+// https://www.youtube.com/watch?v=JvS2triCgOY
+// https://www.youtube.com/watch?v=P8hT5nDai6A
+// https://github.com/CodingTrain/website/blob/main/Courses/intelligence_learning/session4/toy-neural-network-js/examples/xor/sketch.js
 
-const Sketch = () => {
+const Doodle = () => {
     let x = 50;
     const y = 50;
 
     const setup = (p5, canvasParentRef) => {
-        // use parent to render the canvas in this ref
-        // (without that p5 will render the canvas outside of your component)
         p5.createCanvas(500, 500).parent(canvasParentRef);
     };
 
     const draw = (p5) => {
         p5.background(0);
         p5.ellipse(x, y, 70, 70);
-        // NOTE: Do not use setState in the draw function or in functions that are executed
-        // in the draw function...
-        // please use normal variables or class properties for these purposes
         x++;
     };
 
@@ -26,9 +25,31 @@ const Sketch = () => {
 };
 
 function App() {
-    return <div className="App">
-        <Sketch/>
-    </div>
+    return <Container fluid="lg" className="p-0">
+        <Navbar expand="lg" variant="light" bg="light">
+            <Navbar.Brand href="#">Navbar</Navbar.Brand>
+        </Navbar>
+
+        <Row>
+            <Col><Doodle/></Col>
+            <Col>
+                <Card>
+                    <Card.Body>
+                        <Form>
+                            <FormGroup>
+                                <Form.Label>
+                                    Label
+                                </Form.Label>
+                                <InputGroup>
+                                    <Form.Control as="textarea" rows="3" name="address" onChange={() => console.log('s')} />
+                                </InputGroup>
+                            </FormGroup>
+                        </Form>
+                    </Card.Body>
+                </Card>
+            </Col>
+        </Row>
+    </Container>
 }
 
 export default App;
